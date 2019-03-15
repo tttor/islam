7a60f49c44b109f0029a6bd39c76b1c243b10a47
Vektor Dewanto <vektor.dewanto@gmail.com>
2019-03-16 05:50:38 +1000

    fix

--- list.sh
+++ list.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ls -1 -R arabic/ > arabic/listing.txt
+ls -1 -R aqidah/ > aqidah/listing.txt
 ls -1 -R fiqh/ > fiqh/listing.txt
 ls -1 -R lesson/ > lesson/listing.txt
 ls -1 -R quran/ > quran/listing.txt
 ls -1 -R sunnah/ > sunnah/listing.txt
-ls -1 -R tauhid/ > tauhid/listing.txt
 ls -1 -R xtmp/ > xtmp/listing.txt
